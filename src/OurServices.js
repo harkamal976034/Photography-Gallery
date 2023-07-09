6b37ee9d68b2b9f1787e95f26a03b176d88602ca
Vaishali1330 <vaishali.jindal@zoxima.com>
2023-07-09 19:05:50 +0530

    services added

--- src/OurServices.js
+++ src/OurServices.js
@@ -1,11 +1,29 @@
 import React from "react";
+import service from "./assets/service.jpeg";
+import { services } from "./images";
 
 const OurServices = () => {
-    return (
-			<div className="bg-lightBlueBg w-full flex justify-center items-center p-8 md:p-10 flex-col">
-				<h1 className="font-semibold text-2xl md:text-5xl my-4 md:my-10 text-whiteText">Our Services</h1>
+	return (
+		<div className="bg-lightBlueBg w-full flex justify-center items-center p-8 md:p-10 flex-col">
+			<h1 className="font-semibold text-2xl md:text-5xl my-4 md:my-10 text-whiteText">Our Services</h1>
+			<div className="flex flex-wrap justify-center items-start gap-8">
+				{Object.entries(services).map((image) => (
+					<div className="flex flex-col justify-center gap-4  max-w-[400px]">
+						<img
+							key={image[0]}
+							src={image[1].url}
+							alt={image[0]}
+							className="rounded-3xl aspect-square object-cover"
+						/>
+						<div className="flex flex-col justify-center items-center p-2 gap-4">
+							<h1 className="text-2xl text-whiteText font-semibold text-center">{image[0]}</h1>
+							<h5 className="text-lg text-whiteText text-center">{image[1].description}</h5>
+						</div>
+					</div>
+				))}
 			</div>
-		);
+		</div>
+	);
 };
 
 export default OurServices;
--- src/images.js
+++ src/images.js
@@ -1,3 +1,5 @@
+import service from "./assets/service.jpeg";
+
 export const images = {
     "Pre Wedding": ["1EpMqCH9rYHDbHgGv-QkY4xom2LVbGpRL",
         "1EpMqCH9rYHDbHgGv-QkY4xom2LVbGpRL",
@@ -17,4 +19,32 @@ export const images = {
     "Birthday Shoot": ["1EpMqCH9rYHDbHgGv-QkY4xom2LVbGpRL",
         "1EpMqCH9rYHDbHgGv-QkY4xom2LVbGpRL",
         "1EpMqCH9rYHDbHgGv-QkY4xom2LVbGpRL"],
+}
+
+
+export const services = {
+    "Pre Wedding Photography": {
+        url: service,
+        description: "Capture beautiful moments before the big day with our professional pre-wedding photoshoot service, creating stunning memories that will last a lifetime."
+    },
+    "Wedding Photography": {
+        url: service,
+        description: "Preserve the magic of your special day with our exceptional wedding photography services, capturing every cherished moment and creating timeless memories for you to treasure forever."
+    },
+    "Maternity Shoot Photography": {
+        url: service,
+        description: "Embrace the beauty of motherhood with our exquisite maternity photoshoots, capturing the radiant glow and the anticipation of the miracle of life, creating timeless memories to cherish forever."
+    },
+    "Post Wedding Photography": {
+        url: service,
+        description: "Relive the romance and joy of your wedding day with our post-wedding photoshoot service, creating enchanting images that celebrate your love story beyond the big day."
+    },
+    "Baby Shoot Photography": {
+        url: service,
+        description: "Capture the precious moments of your little one's early years with our heartwarming baby photoshoots, preserving the innocence and joy that will melt your heart for a lifetime."
+    },
+    "Birthday Shoot Photography": {
+        url: service,
+        description: "Make their special day even more memorable with our vibrant birthday photoshoots, capturing the joy, excitement, and cherished moments of celebration that will be treasured forever."
+    },
 }
