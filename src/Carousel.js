0df5f380725455e5b06f08fd69244ab8d24122ef
Vaishali1330 <vaishali.jindal@zoxima.com>
2023-07-09 17:22:46 +0530

    image changes

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,14 +3,15 @@ import carousel from "./assets/carousel.jpg";
 
 const Carousel = () => {
 	return (
-		<div className="relative h-64 md:h-auto">
-			<img src={carousel} alt="Rohit Setia Photography" className="object-cover object-center" />
+		<div className="relative h-[28rem] md:h-[540px] bg-[url('./assets/carousel.jpg')] bg-cover bg-center-center" >
+			{/* <img src="https://drive.google.com/uc?export=view&id=1QK8KJctk4ICSt6hjdQsrhTVXj_V9g0W0" alt="Rohit Setia Photography" className="object-cover object-center block md:hidden w-full h-auto" /> */}
 			<div className="absolute bg-darkBlueBg bg-opacity-50 h-full w-full top-0 left-0 flex justify-center items-center flex-col p-6">
 				<h1 className="text-2xl md:text-5xl  text-whiteText font-medium text-center">Capture Life's Best Moments</h1>
 				<h3 className="text-md md:text-xl text-whiteText text-center">
 					Professional photography services capturing the essence of your special moments. Book now for stunning photos!
 				</h3>
 			</div>
+
 		</div>
 	);
 };
--- src/OurWorkGallery.js
+++ src/OurWorkGallery.js
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { images } from "./images";
 
 const OurWorkGallery = () => {
-	const [images, setImages] = useState([]);
-
-	useEffect(() => {
-		const fetchImages = async () => {
-			try {
-				const response = await axios.get(`https://api.cloudinary.com/v1_1/douqdlnf7/resources/image/folder/wedding`, {
-					params: {
-						type: "upload",
-						max_results: 30, // Adjust the number of images to fetch as per your requirement
-						api_key: "399598533712399",
-						api_secret: "VuSxVVRj_BCdzoiODSqPLoHfm04",
-					},
-				});
-				setImages(response.data.resources);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-
-		fetchImages();
-	}, []);
 
 	return (
 		<div>
-			{images.map((image) => (
-				<img key={image.public_id} src={image.secure_url} alt={image.public_id} width="300" height="300" />
+			{Object.entries(images).map((image) => (
+				<img
+					key={image[0]}
+					src={`https://drive.google.com/uc?export=view&id=${image[1][0]}`}
+					alt={image[0]}
+					width="300"
+					height="300"
+				/>
 			))}
 		</div>
 	);
